d742a14e8e5334d4a140f17fce905c1a29ec4add
PratikG <pratik.gokhale1@gmail.com>
2022-12-14 12:18:44 +0530

    final changes to UI

--- chats/views.py
+++ chats/views.py
@@ -5,11 +5,32 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def get_all_chats(request):
     user = request.user
-    chats = Chat.objects.filter(users__username=user.username)
-    return render(request,'chats/display_chats.html',{'chats':chats})
+    chats = Chat.objects.filter(users__username=user.username).order_by('name')
+    if user.is_agent is False:
+        return render(request,'chats/display_chats.html',{'chats':chats})
+    else:
+        data = []
+        for chat in chats:
+            context = {}
+            context['name'] = chat.name
+            context['slug'] = chat.slug
+            for chat_user in chat.users.all():
+                if chat_user.is_agent is False:
+                    context['user_id'] = chat_user.user_id
+            data.append(context)
+        return render(request,'chats/display_chats.html',{'chats':data})
 
 @login_required
 def enter_chat(request,slug):
     chat = Chat.objects.get(slug=slug)
     messages = Message.objects.filter(chat=chat)[:25]
-    return render(request,'chats/single_chat.html',{'chat':chat, 'messages':messages})
+    if request.user.is_agent is False:
+        return render(request,'chats/single_chat.html',{'chat':chat, 'messages':messages})
+    else:
+        context = {}
+        context['name'] = chat.name
+        context['slug'] = chat.slug
+        for chat_user in chat.users.all():
+            if chat_user.is_agent is False:
+                context['user_id'] = chat_user.user_id
+        return render(request,'chats/single_chat.html',{'chat':context, 'messages':messages})
